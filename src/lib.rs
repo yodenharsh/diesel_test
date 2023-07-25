644d473259121e074a4e08448275cbcc8042da82
Harsh Morayya <harshmorayya@ymail.com>
2023-07-25 10:02:03 +0530

    Updated saving post to return ID

--- src/lib.rs
+++ src/lib.rs
@@ -15,12 +15,19 @@ pub fn establish_connection() -> MysqlConnection {
         .unwrap_or_else(|_| panic!("Error connecting to given url"))
 }
 
-pub fn create_post(conn: &mut MysqlConnection, title: &str, body: &str) {
+pub fn create_post(conn: &mut MysqlConnection, title: &str, body: &str) -> Post {
     use crate::schema::posts;
 
     let new_post = NewPost { title, body };
-    diesel::insert_into(posts::table)
-        .values(&new_post)
-        .execute(conn)
-        .expect("lol");
+    conn.transaction(|conn| {
+        diesel::insert_into(posts::table)
+            .values(&new_post)
+            .execute(conn)?;
+
+        posts::table
+            .order(posts::id.desc())
+            .select(Post::as_select())
+            .first(conn)
+    })
+    .expect("Error saving post")
 }
